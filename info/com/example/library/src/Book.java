c9fc5b816648c7718356193a75359c945dd9d16a
kitamreik <kitdamreik@gmail.com>
2025-06-10 00:19:03 -0600

    Exceptions, Student Mgt init and info directory

---
+++ info/com/example/library/src/Book.java
@@ -0,0 +1,69 @@
+package com.example.library;
+
+//When you need to use classes from other packages, you need to import them at the top of your source file. These examples illustrate two examples of importing classes.
+import java.util.ArrayList; // Importing classes from Java's standard library
+import java.util.List;
+
+//Book class- enhanced version - see constructor lab
+public class Book {
+    private String title;
+    private String author;
+    private float price;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public float getPrice() {
+        return this.price;
+    }
+
+    public String toString() {
+        return "Title - " + this.title + "\nAuthor - "
+            + this.author + "\nPrice - "+ String.format("%.2f", this.price);
+    }
+    //When you create an overloaded constructor, another constructor with the same name (using the class name) but different sets of parameters, you need to also create a default constructor.
+    public Book() { // This is the default constructor, which will create an instance but the values will have to be explicitly set with the setters.
+        this.title = null;
+        this.author = null;
+    }
+
+    public Book(String title, String author, float price) { //This is the constructor of the class which takes three parameters for each of the attribute. The value of each of the attribute is set with the parameter passed.
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+}
+
+// Book class - simple version
+/*
+public class Book {
+    private String title;
+    private String author;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public void display() {
+        System.out.println("Title: " + title + ", Author: " + author);
+    }
+}
+*/
---
+++ info/com/example/library/src/BookAccess.java
@@ -0,0 +1,16 @@
+public class BookAccess {
+    public static void main(String s[]) {
+        
+        Book book1 = new Book("Java and Java Chip", "Kit Amreik", 30.00f);
+
+        Book book2 = new Book();
+        book2.setTitle("Java Joy");
+        book2.setAuthor("Kit Amreik");
+        book2.setPrice(25.00f);
+
+        System.out.println("The first book object is ");
+        System.out.println(book1);
+        System.out.println("The second book object is ");
+        System.out.println(book2);
+    }
+}
